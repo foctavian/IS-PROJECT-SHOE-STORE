c8b4dd5eb5d5afeecfe16c71f6fc6d62c431c90b
foctavian <tavi.latavi121@gmail.com>
2023-11-15 17:42:08 +0200

    first commit

---
+++ store/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from .models.product import Product
+from .models.category import Category
+from .models.customer import Customer
+from .models.order import Order
+
+
+class AdminProduct(admin.ModelAdmin):
+    list_display = ['name', 'price', 'category']
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+
+# Register your models here.
+admin.site.register(Product, AdminProduct)
+admin.site.register(Category)
+admin.site.register(Customer)
+admin.site.register(Order)
+
